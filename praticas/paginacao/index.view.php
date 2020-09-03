b7523e74a67bc1a3c36dff643780e8e4ebbe7c1e
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-09-03 20:53:51 -0300

    criando uma lista

---
+++ praticas/paginacao/index.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>pagination</title>
+</head>
+<body>
+    <div class="contenedor">
+      <h1>artigos</h1>
+      <section class="articulos">
+        <ul>
+          <li>1. - Lorem ipsum dolor sit  </li>
+          <li>2. - Lorem ipsum dolor sit  </li>
+          <li>3. - Lorem ipsum dolor sit  </li>
+          <li>4. - Lorem ipsum dolor sit  </li>
+          <li>5. - Lorem ipsum dolor sit  </li>
+        </ul>
+      </section>
+    </div>
+</body>
+</html>
