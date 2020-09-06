a344c23a231cbbd1ec44280a13941553781329cc
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-09-06 15:25:17 -0300

    show rows

--- praticas/mysqli/conexion.php
+++ praticas/mysqli/conexion.php
@@ -3,7 +3,9 @@ $conexion = new mysqli('localhost','bruno','bruno123','prueba_datos');
 if ($conexion->connect_errno){
   die('lo siento');
 } else{
-  
+  $sql = 'select * from usuarios';
+  $resultado = $conexion->query($sql);
+  echo $resultado->num_rows;
 }
 
 ?>
